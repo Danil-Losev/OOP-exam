99e10a51835527754d781af3009165e459deb1a2
Danil Losev <dlosevdan@gmail.com>
2025-06-12 22:16:28 +0600

    Добавьте файлы проекта.

---
+++ 3/three/three/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace three
+{
+    public class Book
+    {
+        private string bookName;
+        private string authorName;
+        private string publisherName;
+        private int yearPublished;
+        private int numberOfPages;
+
+        public Book()
+        {
+            bookName = "Unknown";
+            authorName = "Unknown";
+            publisherName = "Unknown";
+            yearPublished = 0;
+            numberOfPages = 0;
+        }
+
+        public Book(string inBookName, string inAuthorName, string inPublisherName, int inYearPublished, int inNumberOfPages)
+        {
+            bookName = inBookName;
+            authorName = inAuthorName;
+            publisherName = inPublisherName;
+            yearPublished = inYearPublished;
+            numberOfPages = inNumberOfPages;
+        }
+
+        public string GetBookName()
+        {
+            return bookName;
+        }
+
+        public string GetAuthorName()
+        {
+            return authorName;
+        }
+
+        public string printBookInfo()
+        {
+            return "Book name:" + bookName + " , " + "Author name:" + authorName + " , " +
+                   "Publisher name:" + publisherName + " , " + "Year published:" + yearPublished + " , " +
+                   "Number of pages:" + numberOfPages;
+        }
+    }
+}
---
+++ 3/three/three/BookShell.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace three
+{
+    public class BookShell
+    {
+        private string shellName;
+        private List<Book> bookShell;
+
+        public BookShell()
+        {
+            shellName = "Unknown";
+            bookShell = new List<Book>();
+        }
+
+        public BookShell(string inShellName, List<Book> inBookShell)
+        {
+            shellName = inShellName;
+            bookShell = inBookShell;
+        }
+
+
+        public Book SearchByAuthor(string authorName)
+        {
+            if (bookShell.Count != 0) foreach (Book book in bookShell)
+                {
+                    if (book.GetAuthorName() == authorName)
+                    {
+                        return book;
+                    }
+                }
+            Console.WriteLine("Book not found");
+            return new Book();
+        }
+
+        public Book SearchByName(string bookName)
+        {
+            if (bookShell.Count != 0) foreach (Book book in bookShell)
+                {
+                    if (book.GetBookName() == bookName)
+                    {
+                        return book;
+                    }
+                }
+            Console.WriteLine("Book not found");
+            return new Book();
+        }
+
+        public void AddBook(Book book)
+        {
+            if (bookShell.Contains(book)) Console.WriteLine("Book already exists in the shell.");
+            else bookShell.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if (bookShell.Contains(book)) bookShell.Remove(book);
+            else Console.WriteLine("Book not found in the shell.");
+        }
+
+
+        public void PrintShell()
+        {
+            Console.WriteLine("Shell Name: " + shellName);
+            Console.WriteLine("Books in the shell:");
+            foreach (Book book in bookShell)
+            {
+                book.printBookInfo();
+            }
+        }
+    }
+}
